16f314adf6c7c11935e62fc27a526cc9c06ec20b
Brian Huo <huoshengyu@berkeley.edu>
2024-06-04 18:18:42 -0700

    UR ROS support - first commit

---
+++ gello/robots/ur_ros.py
@@ -0,0 +1,137 @@
+from typing import Dict
+
+import numpy as np
+
+from gello.robots.robot import Robot
+
+# ROS compatibility edits
+import rospy
+import sensor_msgs.msg
+import trajectory_msgs.msg
+import std_msgs.msg
+
+
+class URRobot(Robot):
+    """A class representing a UR robot."""
+
+    def __init__(self, robot_ip: str = "192.168.1.10", no_gripper: bool = False):
+        
+        if not no_gripper:
+            from gello.robots.robotiq_gripper import RobotiqGripper
+
+            self.gripper = RobotiqGripper()
+            self.gripper.connect(device="/tmp/ttyUR")
+            print("gripper connected")
+            # gripper.activate()
+
+        [print("connect") for _ in range(4)]
+
+        self._free_drive = False
+        self.robot.endFreedriveMode()
+        self._use_gripper = not no_gripper
+
+        # ROS compatibility edits
+        rospy.init_node('gello_ros_control')
+
+        joint_state_topic = "/joint_states"
+        # joint_traj_topic = "/pos_joint_traj_controller/command"
+        joint_pos_topic = "/joint_group_pos_controller/command"
+
+        self._joint_state_subscriber = rospy.Subscriber(joint_state_topic, sensor_msgs.msg.JointState, self._joint_state_sub_callback)
+        # self._joint_traj_publisher = rospy.Publisher(joint_traj_topic, trajectory_msgs.msg.JointTrajectory, queue_size=1)
+        self._joint_pos_publisher = rospy.Publisher(joint_pos_topic, std_msgs.msg.Float64MultiArray, queue_size=1)
+
+        self._joint_state = None
+
+    def _joint_state_sub_callback(self, message):
+        self._joint_state = message
+
+    def num_dofs(self) -> int:
+        """Get the number of joints of the robot.
+
+        Returns:
+            int: The number of joints of the robot.
+        """
+        if self._use_gripper:
+            return 7
+        return 6
+
+    def _get_gripper_pos(self) -> float:
+        import time
+
+        time.sleep(0.01)
+        gripper_pos = self.gripper.get_current_position()
+        assert 0 <= gripper_pos <= 255, "Gripper position must be between 0 and 255"
+        return gripper_pos / 255
+
+    def get_joint_state(self) -> np.ndarray:
+        """Get the current state of the leader robot.
+
+        Returns:
+            T: The current state of the leader robot.
+        """
+        robot_joints = [x for x in self._joint_state] # copy to prevent list mutation 
+        if self._use_gripper:
+            gripper_pos = self._get_gripper_pos()
+            pos = np.append(robot_joints, gripper_pos)
+        else:
+            pos = robot_joints
+        return pos
+
+    def command_joint_state(self, joint_state: np.ndarray) -> None:
+        """Command the leader robot to a given state.
+
+        Args:
+            joint_state (np.ndarray): The state to command the leader robot to.
+        """
+        robot_joints = joint_state[:6]
+        robot_joints_msg = std_msgs.msg.Float64MultiArray()
+        robot_joints_msg.data = robot_joints
+        self._joint_pos_publisher.publish(robot_joints_msg)
+        
+        if self._use_gripper:
+            gripper_pos = joint_state[-1] * 255
+            self.gripper.move(gripper_pos, 255, 10)
+
+    def freedrive_enabled(self) -> bool:
+        """Check if the robot is in freedrive mode.
+
+        Returns:
+            bool: True if the robot is in freedrive mode, False otherwise.
+        """
+        return self._free_drive
+
+    def set_freedrive_mode(self, enable: bool) -> None:
+        """Set the freedrive mode of the robot.
+
+        Args:
+            enable (bool): True to enable freedrive mode, False to disable it.
+        """
+        if enable and not self._free_drive:
+            self._free_drive = True
+            self.robot.freedriveMode()
+        elif not enable and self._free_drive:
+            self._free_drive = False
+            self.robot.endFreedriveMode()
+
+    def get_observations(self) -> Dict[str, np.ndarray]:
+        joints = self.get_joint_state()
+        pos_quat = np.zeros(7)
+        gripper_pos = np.array([joints[-1]])
+        return {
+            "joint_positions": joints,
+            "joint_velocities": joints,
+            "ee_pos_quat": pos_quat,
+            "gripper_position": gripper_pos,
+        }
+
+
+def main():
+    ur = URRobot(no_gripper=True)
+    print(ur)
+    ur.set_freedrive_mode(True)
+    print(ur.get_observations())
+
+
+if __name__ == "__main__":
+    main()
