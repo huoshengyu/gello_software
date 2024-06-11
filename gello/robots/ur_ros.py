910bc18f42c6686560fcfd1fb062c6cc9f28605c
Brian Huo <huoshengyu@berkeley.edu>
2024-06-11 16:08:12 -0700

    Joints sorted

--- gello/robots/ur_ros.py
+++ gello/robots/ur_ros.py
@@ -63,8 +63,9 @@ class URRobot(Robot):
         Returns:
             T: The current state of the leader robot.
         """
-        robot_joints = [x for x in self._joint_state] # copy to prevent list mutation
-        pos = robot_joints
+        robot_joints = [x for x in self._joint_state.position] # copy to prevent list mutation
+        ros_joint_indices = [3, 2, 0, 4, 5, 6, 1] # order in which joint_states message must be indexed to sort joints from base to gripper
+        pos = [robot_joints[i] for i in ros_joint_indices] # rearrange joints to be in order from base to gripper
         return pos
 
     def command_joint_state(self, joint_state: np.ndarray) -> None:
